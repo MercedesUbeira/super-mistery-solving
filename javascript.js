d9a1dcd883504ddc268398fcfdd31275ff8024a1
Mercedes Ubeira Romero <mercedesubeira@gmail.com>
2023-11-22 21:25:37 +0100

    Bugs fixed. Added pic of the suspect

--- javascript.js
+++ javascript.js
@@ -1,56 +1,62 @@
 function solveMystery() {
-    var whoInput = document.getElementById('who');
-    var howInput = document.getElementById('how');
-    var whyInput = document.getElementById('why');
-    var whoError = document.getElementById('who-error');
-    var howError = document.getElementById('how-error');
-    var whyError = document.getElementById('why-error');
-    var congratulations = document.getElementById('congratulations');
-  
-    // Reset styles
-    whoInput.classList.remove('correct', 'error');
-    howInput.classList.remove('correct', 'error');
-    whyInput.classList.remove('correct', 'error');
-    whoError.textContent = '';
-    howError.textContent = '';
-    whyError.textContent = '';
-  
-    // Check answers
-    var correct = true;
-  
-    if (whoInput.value.toLowerCase() !== 'richard') {
+  var whoInput = document.getElementById('who');
+  var howInput = document.getElementById('how');
+  var whyInput = document.getElementById('why');
+  var whoError = document.getElementById('who-error');
+  var howError = document.getElementById('how-error');
+  var whyError = document.getElementById('why-error');
+  var congratulations = document.getElementById('congratulations');
+
+  // Reset styles
+  whoInput.classList.remove('correct', 'error');
+  howInput.classList.remove('correct', 'error');
+  whyInput.classList.remove('correct', 'error');
+  whoError.textContent = '';
+  howError.textContent = '';
+  whyError.textContent = '';
+
+  // Check answers
+  var correct = true;
+
+  if (whoInput.value.toLowerCase() !== 'chemist') {
       whoInput.classList.add('error');
       whoError.textContent = 'Wrong information, please try again.';
       correct = false;
-    } else {
+  } else {
       whoInput.classList.add('correct');
       whoInput.setAttribute('readonly', true);
-    }
-  
-    if (howInput.value.toLowerCase() !== 'poison') {
+  }
+
+  if (howInput.value.toLowerCase() !== 'poison') {
       howInput.classList.add('error');
       howError.textContent = 'Wrong information, please try again.';
       correct = false;
-    } else {
+  } else {
       howInput.classList.add('correct');
       howInput.setAttribute('readonly', true);
-    }
-  
-    if (whyInput.value.toLowerCase() !== 'inheritance') {
+  }
+
+  if (whyInput.value.toLowerCase() !== 'inheritance') {
       whyInput.classList.add('error');
       whyError.textContent = 'Wrong information, please try again.';
       correct = false;
-    } else {
+  } else {
       whyInput.classList.add('correct');
       whyInput.setAttribute('readonly', true);
-    }
-  
-    // Display a general error message if all fields are incorrect
-    if (!correct) {
-      whoError.textContent = 'Wrong information, please try again.';
-    } else {
-      // Show congratulations message
+  }
+
+  if (!correct) {
+      if (whoInput.value.toLowerCase() !== 'chemist') {
+          whoError.textContent = 'Wrong information, please try again.';
+      }
+      if (howInput.value.toLowerCase() !== 'poison') {
+          howError.textContent = 'Wrong information, please try again.';
+      }
+      if (whyInput.value.toLowerCase() !== 'inheritance') {
+          whyError.textContent = 'Wrong information, please try again.';
+      }
+  } else {
       document.getElementById('mystery-form').style.display = 'none';
       congratulations.style.display = 'block';
-    }
   }
+}
